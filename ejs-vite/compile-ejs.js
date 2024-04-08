7bc9f823e51b072f3de9620f60fc7d421a90ace7
838861hs <philoaki0@gmail.com>
2024-04-08 17:36:07 +0900

    feat:ejsファイルの文頭に_がある時はコンパイルなし

--- ejs-vite/compile-ejs.js
+++ ejs-vite/compile-ejs.js
@@ -12,7 +12,14 @@ const distDir = path.join(__dirname, 'dist');
 
 const compileEJS = (file) => {
   const fileName = path.basename(file, '.ejs');
-  ejs.renderFile(file, {}, {}, (err, str) => {
+
+  // ファイル名が "_" で始まる場合はコンパイルをスキップ
+  if (fileName.startsWith('_')) {
+    console.log(`Skipping partial: ${fileName}.ejs`);
+    return;
+  }
+
+  ejs.renderFile(file, {}, {}, (err, html) => {
     if (err) {
       console.error(err);
       return;
@@ -20,14 +27,20 @@ const compileEJS = (file) => {
     if (!fs.existsSync(distDir)) {
       fs.mkdirSync(distDir, { recursive: true });
     }
-    fs.writeFileSync(path.join(distDir, `${fileName}.html`), str);
+    fs.writeFileSync(path.join(distDir, `${fileName}.html`), html);
     console.log(`${fileName}.html has been compiled`);
   });
 };
 
+// src/ejs ディレクトリ内の EJS ファイルの変更を監視
 chokidar.watch(`${srcDir}/**/*.ejs`).on('change', (file) => {
   console.log(`Detected change in ${file}, recompiling...`);
   compileEJS(file);
 });
 
-compileEJS(path.join(srcDir, 'index.ejs'));
+// 初期起動時に条件に一致する全EJSファイルをコンパイル
+fs.readdirSync(srcDir).forEach(file => {
+  if (path.extname(file) === '.ejs' && !path.basename(file).startsWith('_')) {
+    compileEJS(path.join(srcDir, file));
+  }
+});
